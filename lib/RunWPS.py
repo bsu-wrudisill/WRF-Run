46d0831fe8b28431b19fa5ef09d0cacebf87dbd0
bsu-wrudisill <williamrudisill@u.boisestate.edu>
2019-11-16 11:02:08 -0700

    add metgrid run up to submission

--- lib/RunWPS.py
+++ lib/RunWPS.py
@@ -101,7 +101,6 @@ class RunWPS(SetMeUp):
 		# Fixed paths -- these should get created 
 		cwd = os.getcwd() 
 		vtable = self.ungrib_run_dirc.joinpath('Vtable')
-		submit_script = self.ungrib_run_dirc.joinpath('submit_ungrib.sh')
 		ungrib_log = self.ungrib_run_dirc.joinpath('ungrib.log')
 		namelist_wps = self.ungrib_run_dirc.joinpath('namelist.wps')        
 		linkGrib = '{}/link_grib.csh {}/{}' # CHANGE ME 
@@ -112,7 +111,7 @@ class RunWPS(SetMeUp):
 		# Get pbs submission parameters and create submit command 
 		queue = kwargs.get('queue', self.queue)                        # get the queue 
 		qp = kwargs.get('queue_params', self.queue_params.get('wps'))  # get the submit parameters               
-		submit_script = kwargs.get('submit_script', self.geo_run_dirc.joinpath('submit_geogrid.sh')) 
+		submit_script = kwargs.get('submit_script', self.ungrib_run_dirc.joinpath('submit_ungrib.sh')) 
 		
 		# Form the job submission command 
 		lines = ["source %s" %self.environment_file,
@@ -126,7 +125,6 @@ class RunWPS(SetMeUp):
 			       "LOGNAME":"ungrib-PLEVS",
 			       "CMD": command
 			       }
-		
 		
 		
 		# Adjust parameters in the namelist.wps template script 
@@ -236,6 +234,88 @@ class RunWPS(SetMeUp):
 			os.unlink(globfile)
 		# check that the script finished correctly
 		os.chdir(cwd)
+	
+	@acc.timer
+	def metgrid(self, **kwargs):
+		# ----------------------------------------------
+		# kwarg options
+		# 
+		#  ungrib_location: (path to SFLUX/PLEVS files)
+		#  geogrid_location: (path to geo_em* files)
+		#  --force: whether or not to clean out the metgrid directory or not  
+		# ----------------------------------------------
+
+
+		# Start logging
+		logger = logging.getLogger(__name__)
+		logger.info('entering metgrib process in directory')
+		logger.info('WRF Version {}'.format(self.wrf_version))
+		
+		# 
+		clean = kwargs.get('force', True)  #whether or not to 'clean' the dir before running 
+
+		# Fixed paths -- these should get created 
+		cwd = os.getcwd() 
+		metgrid_log = self.met_run_dirc.joinpath('metgrid.log')
+		namelist_wps = self.met_run_dirc.joinpath('namelist.wps')        
+		catch_id = 'metgrid.catch'
+		unique_name = 'm_{}'.format(secrets.token_hex(2))
+		success_message = 'Successful completion of program metgrid.exe'
+		
+		# verify that we are 'metgrid ready'
+		#if self.ungrib_success and self.geogrid_success:
+		#	logger.info('metgrid ready -- procede')
+		#else: 
+			# maybe the satus hasn't been updated yet, or it's 
+			# not ready. check anyway
+			# acc.file_check(	
+		#	pass
+	
+		# link ungrib files 
+		logger.info('Creating symlink for SFLUX')
+		for sflux in self.ungrib_run_dirc.glob('SFLUX*'):
+			os.symlink(sflux, self.met_run_dirc.joinpath(sflux.name))
+		
+		logger.info('Creating symlink for PLEVS')
+		for plevs in self.ungrib_run_dirc.glob('PLEVS*'):
+			os.symlink(sflux, self.met_run_dirc.joinpath(plevs.name))
+
+		# link geogrid files 
+		for geo_em in self.geo_run_dirc.glob('geo_em.d0?.nc'):
+			logger.info('Creating symlink...')
+			logger.info('Found {} in {}'.format(geo_em.name, self.geo_run_dirc))
+			os.symlink(geo_em, self.met_run_dirc.joinpath(geo_em.name))
+
+		
+		# Get pbs submission parameters and create submit command 
+		queue = kwargs.get('queue', self.queue)                        # get the queue 
+		qp = kwargs.get('queue_params', self.queue_params.get('wps'))  # get the submit parameters               
+		submit_script = self.met_run_dirc.joinpath('submit_metgrid.sh')
+		
+		# Form the job submission command 
+		lines = ["source %s" %self.environment_file,
+			 "cd %s" %self.met_run_dirc,
+			 "./metgrid.exe &> metgrid.catch"]
+		command = "\n".join(lines)  # create a single string separated by spaces
+		
+		# Create the run script based on the type of job scheduler system  
+		replacedata = {"QUEUE":queue,
+			       "JOBNAME":unique_name,
+			       "LOGNAME":"metgrid",
+			       "CMD": command
+			       }
+		
+		# Adjust parameters in the namelist.wps template script 
+		wps_replace_dic = {"GEOG_PATH":self.geog_data_path,
+      			           "GEOG_TBL_PATH":self.geo_exe_dirc,
+			           "METGRID_TBL_PATH":self.met_exe_dirc,
+			           "startdate":self.start_date.strftime(self.time_format),
+			           "enddate":self.end_date.strftime(self.time_format),
+			           }
+		
+		# Navigate to the metgrid director. !!! NOT SURE IF WE NEED TO DO THIS !!!
+		acc.GenericWrite(self.main_run_dirc.joinpath('namelist.wps.template'), wps_replace_dic, namelist_wps)
+		acc.WriteSubmit(qp, replacedata, filename=submit_script)
 
 	@acc.timer
 	def dataDownload(self):
@@ -244,7 +324,7 @@ class RunWPS(SetMeUp):
 		sub6 = datetime.timedelta(hours=6)
 		date_range = pd.date_range(self.start_date - sub6, self.end_date, freq='6H')
 		self.file_spec = '06.gdas'
-		#### 
+		# 
 		if self.lbc_type == 'cfsr': 
 			nomads_url = "https://nomads.ncdc.noaa.gov/modeldata/cmd_{}/{}/{}{}/{}{}{}/"
 		else:
