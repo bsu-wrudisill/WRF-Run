b258b9657b4a9a461f36a9806770b4c18ab4823c
bsu-wrudisill <williamrudisill@u.boisestate.edu>
2019-11-23 11:51:28 -0700

    stay updated

--- test.py
+++ test.py
@@ -3,6 +3,7 @@ import logging, logging.config
 import datetime 
 import sys 
 import pathlib
+import argparse 
 libPathList = ['./lib/']
 for libPath in libPathList:
 		sys.path.insert(0,libPath)
@@ -11,31 +12,33 @@ for libPath in libPathList:
 from SetMeUp import SetMeUp
 from RunWPS import RunWPS
 from RunWRF import RunWRF
+from checks import RunPreCheck
 
 
-
-if __name__ == '__main__':
-	#--- logger stuff ---- 	
-	suffix = datetime.datetime.now().strftime("%Y-%m-%d_%H%M%S")
-	logging.config.fileConfig('logger.ini', defaults={'date':suffix})
-	logger = logging.getLogger(__name__)
-	logger.info('Starting...')
-	
-	# Main Config file
-	main = pathlib.Path('user_config/main.yml')
+#--- logger stuff ---- 	
+suffix = datetime.datetime.now().strftime("%Y-%m-%d_%H%M%S")
+logging.config.fileConfig('./user_config/logger.ini', defaults={'date':suffix})
+logger = logging.getLogger(__name__)
+logger.info('Starting...')
 
 
-	#--- done loggering --- 
-	setup = SetMeUp(main)
-	setup.createRunDirectory()
-	wps = RunWPS('main.yml')  
-	wps.geogrid()
+# Begin Setup
+main = pathlib.Path('user_config/main.yml')
+setup = SetMeUp(main)
 
-	#wps.dataDownload()
-	#wps.ungrib()
-	#wps.metgrid()
-	#
-	##logger.info("--------STARTING WRF-------")
-	#wrf = RunWRF('main.yml')
-	#wrf.SetupRunFiles()
-	#wrf.WRF_TimePeriod()
+# Perform some preliminary checks 
+checks = RunPreCheck(main)
+checks.run_all()
+setup.createRunDirectory()
+
+# Begin WPS
+wps = RunWPS(main)
+wps.geogrid()
+wps.dataDownload()
+wps.ungrib()
+wps.metgrid()
+
+# Begin WRF
+wrf = RunWRF(main, wps=wps)
+wrf.SetupRunFiles()
+wrf.WRF_TimePeriod()
